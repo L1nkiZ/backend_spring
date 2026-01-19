84d7e436d758d35b790bcd973cab697b374181da
Guillaume G <guillaume.gerard2112@outlook.fr>
2026-01-19 12:26:17 +0100

    Add HomeRequest class and implement post method in HomeController

--- src/main/java/LinkiZ/Ynov/demo/controllers/HomeController.java
+++ src/main/java/LinkiZ/Ynov/demo/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package LinkiZ.Ynov.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import LinkiZ.Ynov.demo.payload.responses.HomeResponse;
+
 
 
 @RestController
@@ -16,5 +19,11 @@ public class HomeController {
     public String home() {
         return homeResponse().getValue();
     }
+
+    @PostMapping("/")
+    public HomeResponse postMethodName(@RequestBody String param) {
+        return new HomeResponse(param);
+    }
+    
     
 }
