4a07e9fb233bb4bb3048f35833d3a2270391ad64
Guillaume G <guillaume.gerard2112@outlook.fr>
2026-01-19 15:40:18 +0100

    fix : Methode dans le NoteController + rename CrudNoteController en NoteController

--- src/main/java/LinkiZ/Ynov/demo/controllers/NoteController.java
+++ src/main/java/LinkiZ/Ynov/demo/controllers/NoteController.java
@@ -3,7 +3,10 @@ package LinkiZ.Ynov.demo.controllers;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,7 @@ import LinkiZ.Ynov.demo.model.Note;
 
 @RestController
 @RequestMapping("/notes")
-public class CrudNoteController {
+public class NoteController {
     private List<Note> notes = new ArrayList<>();
 
     @PostMapping("/add")
@@ -25,9 +28,9 @@ public class CrudNoteController {
         return "La note a été ajoutée avec succès";
     }
 
-    @PostMapping("/remove")
-    public String removeNote(@RequestBody Note noteRequest) {
-        notes.removeIf(note -> note.getId() == noteRequest.getId());
+    @DeleteMapping("/remove/{id}")
+    public String removeNote(@PathVariable Integer id) {
+        notes.removeIf(note -> note.getId() == id);
         return "La note a été supprimée avec succès";
     }
 
@@ -46,7 +49,7 @@ public class CrudNoteController {
         return null;
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public String updateNote(@RequestBody Note noteRequest) {
         for (Note note : notes) {
             if (note.getId() == noteRequest.getId()) {
