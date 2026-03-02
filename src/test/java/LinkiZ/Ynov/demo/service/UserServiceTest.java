caf6b23abd5284d61b3d399ea56aea4039301a29
Guillaume G <guillaume.gerard2112@outlook.fr>
2026-03-02 12:16:29 +0100

    test : ajout d'un test pour testLoadUserByUsername avec login mdp et role ; test unitaire

---
+++ src/test/java/LinkiZ/Ynov/demo/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package LinkiZ.Ynov.demo.service;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import LinkiZ.Ynov.demo.entity.Role;
+import LinkiZ.Ynov.demo.entity.User;
+import LinkiZ.Ynov.demo.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void testLoadUserByUsername() {
+        // Arrange
+        UserService userService = new UserService(userRepository); //préparation du service et du comportement du mock
+
+        String username = "testuser";
+        User user = new User();
+        Role role = new Role();
+        role.setName("USER");
+
+        user.setUsername(username);
+        user.setPassword("password");
+        // Ajouter le rôle à la liste (User#addRole) pour que UserService récupère les roles
+        user.addRole(role);
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user)); // Le mock renverra l'utilisateur simulé lorsqu'on cherchera par username
+
+        // Act
+        UserDetails userDetails = userService.loadUserByUsername(username); //Appel de la methode
+
+        // Assert
+        // Vérification que le résultat n'est pas null et que le username correspond à celui de l'utilisateur simulé
+        assertThat(userDetails)
+            .isNotNull()
+            .extracting(UserDetails::getUsername)
+            .isEqualTo(username);
+    }
+}
